b5cb1622a59d820205e8035395996b084b54f917
ospFPS <119087612+ospFPS@users.noreply.github.com>
2023-02-04 10:09:20 +0700

    Add files via upload

---
+++ Health problem analyze DEMO .ver/HTML/index.js
@@ -0,0 +1,13 @@
+const { json } = require("express/lib/response");
+function togglepopup(){
+    document.getElementById("popup-1")
+    .classlist.toggle("active");
+} 
+var gender ="male";
+var age ="13";
+
+if (gender == "male"){
+    if (age >= 7) [
+        console.log("You might have Covid-19")
+    ]
+}
